fb8b856d3cc908d30b40727e2ce316df95f1870e
quarterMoon234 <quarterMoon234@gmail.com>
2024-08-26 22:07:08 +0900

    first commite

---
+++ src/main/java/com/example/sns/SnsApplication.java
@@ -0,0 +1,15 @@
+package com.example.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsApplication.class, args);
+        System.out.println("1");
+    }
+
+}
